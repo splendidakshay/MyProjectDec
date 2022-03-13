47b217c5029782c53b5e73165c0ec9ebe54095ea
57694_Prathmesh <prathmeshligade49@gmail.com>
2022-03-13 08:19:53 +0530

    for demo purpose prathmesh have added one code

---
+++ src/main/java/baseclasses/demoforgit.java
@@ -0,0 +1,14 @@
+package baseclasses;
+
+public class demoforgit {
+
+	public static void main(String[] args) {
+	
+	System.out.println("its sunday mrning and having practice on git for pr request");
+	System.out.println("whenever added new code have to create new branch ");
+	System.out.println("and after that from local repo publish it");
+	
+}
+	
+	
+}
